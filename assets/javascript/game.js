503b86da6ea38741e67a87ba4572da92812ebf2f
Aakrati Goel <aakratigoel@gmail.com>
2019-07-02 16:45:29 -0700

    removed wins increment functionality and resetting of values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,23 @@
-
 var wins = document.getElementById("win1");
 var nog = document.getElementById("nogr");
+var lgc = document.getElementById("lgc1");
 var cw = document.getElementById("currentWord1");
 var lag1 =document.getElementById("lag");
-var stringMatch="";
 var totalWins =0;
 var numberOfGuesses = 12;
 console.log(numberOfGuesses);
 nog.textContent=numberOfGuesses;
 var wordArray=["think","special","assume","thought"];
 var computerGeneratedWord;
+
 computerGeneratedWord=wordArray[Math.floor(Math.random()*wordArray.length)];
+
 var letterGuessed= new Array();
+var letterGuessedCorrectly= new Array();
 var userInput;
-//var emptyAr = new Array();
-//var isFound= false;
-//var count =0;
+var emptyAr = new Array();
+var isFound= false;
+var count =0;
 //cw.textContent= computerGeneratedWord;
 console.log(computerGeneratedWord);
 var lengthOfGeneratedWord=computerGeneratedWord.length;
@@ -28,16 +30,15 @@ cw.textContent=dashedArray;
 
 document.onkeyup=function(event)
 {
-  
   if(numberOfGuesses === 0)
   {
     alert("Game is over , click okay to get a new word");
     computerGeneratedWord=wordArray[Math.floor(Math.random()*wordArray.length)];
     dashedArray.length=computerGeneratedWord.length;
     for(var k =0;k<dashedArray.length;k++)
-    {
-      dashedArray[k]="_";
-    }
+{
+  dashedArray[k]="_";
+}
     cw.textContent= dashedArray;
     letterGuessed=[];
     lag1.textContent=letterGuessed;
@@ -45,11 +46,11 @@ document.onkeyup=function(event)
     nog.textContent=numberOfGuesses;
     return;
   }
-  
-                
+
+
     userInput= event.key;
    console.log(userInput);
-   
+
     for(var i =0;i<lengthOfGeneratedWord;i++)
     {
       for(var j=0;j<letterGuessed.length;j++)
@@ -59,46 +60,18 @@ document.onkeyup=function(event)
                       return;
                     }
                 }
-             
+
             if(userInput===computerGeneratedWord.charAt(i))
             {
                 console.log("Entered the if");
                 dashedArray[i]=userInput;
                 cw.textContent=dashedArray;
-                stringMatch=stringMatch+dashedArray[i];
-                if(stringMatch.length === computerGeneratedWord.length)
-                {
-                  numberOfGuesses--;
-                  nog.textContent=numberOfGuesses;
-                  letterGuessed.push(userInput);   
-                  lag1.textContent=letterGuessed; 
-                  console.log("You win");
-                  totalWins++;
-                  wins.textContent=totalWins;
-                // alert("You've won!!, click okay to get a new word");
-                  computerGeneratedWord=wordArray[Math.floor(Math.random()*wordArray.length)];
-                  dashedArray.length=computerGeneratedWord.length;
-                  for(var k =0;k<dashedArray.length;k++)
-                  {
-                    dashedArray[k]="_";
-                  }
-                  cw.textContent= dashedArray;
-                  letterGuessed=[];
-                  lag1.textContent=letterGuessed;
-                  numberOfGuesses=12;
-                  nog.textContent=numberOfGuesses;
-                  stringMatch="";
-                  return;
 
-                }
-                
             }                 
     } 
-    
+
      numberOfGuesses--;
      nog.textContent=numberOfGuesses;
      letterGuessed.push(userInput);   
-     lag1.textContent=letterGuessed; 
-     
+     lag1.textContent=letterGuessed;  
 }
-
